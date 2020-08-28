b1aba556f934dd1d2c5808289fbe6d0745161de6
Mateus Soares da Rocha <rocha.mateus96@gmail.com>
2020-08-28 16:19:53 -0300

    Remove duplicate vars

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,35 +1,6 @@
 import express from "express";
 import lineController from '../controllers/lineController'
 
-const users = [
-  {
-    id: 1,
-    name: "Steve",
-    email: "Brown@gmail.com",
-    gender: "Masculino",
-  },
-  {
-    id: 2,
-    name: "Janine",
-    email: "Smith@gmail.com",
-    gender: "Feminino",
-  },
-  {
-    id: 3,
-    name: "Karen",
-    email: "Jones@gmail.com",
-    gender: "Feminino",
-  },
-  {
-    id: 4,
-    name: "Ben",
-    email: "William@gmail.com",
-    gender: "Masculino",
-  },
-];
-
-const line = [];
-
 const indexRouter = express.Router();
 
 indexRouter.get("/users/:id", (req, res) =>
